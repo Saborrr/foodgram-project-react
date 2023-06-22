9f5e7670707ac238a6ecded3b5a4861f7823bc5d
Александр Фадеев <dj-soft@mail.ru>
2023-06-23 00:45:43 +0300

    Verbose name, static/admin/, conf.d

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,19 +5,20 @@ from django.db import models
 class User(AbstractUser):
     """Модель пользователя."""
 
-    email = models.EmailField(
-        "Электронная почта",
-        max_length=254,
-        unique=True,
-        null=False,)
+    email = models.EmailField(verbose_name="Электронная почта",
+                              max_length=254,
+                              unique=True,
+                              null=False,)
     username = models.CharField(
         max_length=150,
         unique=True,
         null=False)
-    first_name = models.CharField(
-        "Имя пользователя", max_length=150, blank=True)
-    last_name = models.CharField(
-        "Фамилия пользователя", max_length=150, blank=True)
+    first_name = models.CharField(verbose_name="Имя пользователя",
+                                  max_length=150,
+                                  blank=True)
+    last_name = models.CharField(verbose_name="Фамилия пользователя",
+                                 max_length=150,
+                                 blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
